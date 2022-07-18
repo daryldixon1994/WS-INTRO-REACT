6c3f1f5fa7ffa5379bceebcca331fc9a848c7127
daryldixon1994 <kmohamedanouar@gmail.com>
2022-07-18 12:00:21 +0100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Button variant="primary">Submit</Button>
+        </div>
+    );
+}
+
+export default Home;
